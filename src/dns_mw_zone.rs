85a4601abb98c0dc10b00a353c2d9ef364b219f8
YISH <mokeyish@hotmail.com>
2023-03-26 21:16:19 +0800

    :art: Optimize arpa

--- src/dns_mw_zone.rs
+++ src/dns_mw_zone.rs
@@ -1,46 +1,64 @@
+use std::borrow::Borrow;
 use std::collections::{BTreeSet, HashMap};
+use std::net::IpAddr;
 use std::str::FromStr;
 
+use ipnet::IpNet;
 use trust_dns_proto::rr::rdata::PTR;
 use trust_dns_proto::rr::LowerName;
 use trust_dns_server::authority::{AuthorityObject, LookupOptions};
 
 use crate::dns::*;
 use crate::dns_conf::SmartDnsConfig;
+use crate::infra::ipset::IpSet;
 use crate::log::debug;
 use crate::middleware::*;
 
 pub struct DnsZoneMiddleware {
     catalog: Catalog,
-    ptr_set: BTreeSet<LowerName>,
+    server_net: IpSet,
+    server_names: BTreeSet<Name>,
 }
 
 impl DnsZoneMiddleware {
     pub fn new(_cfg: &SmartDnsConfig) -> Self {
         let catalog = Catalog::new();
 
-        let ptr_set = {
-            let mut set = BTreeSet::<LowerName>::new();
-            set.insert(Name::from_str("whoami").unwrap().into());
-            set.insert(Name::from_str("smartdns").unwrap().into());
+        let server_net = {
+            let mut ips = Vec::<IpAddr>::new();
 
             #[cfg(not(target_os = "android"))]
             {
-                use crate::third_ext::IpAddrToArpa;
                 use local_ip_address::list_afinet_netifas;
                 if let Ok(network_interfaces) = list_afinet_netifas() {
                     for (_, ip) in network_interfaces.iter() {
-                        if let Ok(n) = Name::from_str(ip.to_arpa().as_str()) {
-                            set.insert(n.into());
-                        }
+                        ips.push(*ip);
                     }
                 }
             }
 
+            IpSet::new(
+                ips.into_iter()
+                    .map(|ip| match ip {
+                        IpAddr::V4(_) => IpNet::new(ip, 32).unwrap(),
+                        IpAddr::V6(_) => IpNet::new(ip, 128).unwrap(),
+                    })
+                    .collect(),
+            )
+        };
+
+        let server_names = {
+            let mut set = BTreeSet::new();
+            set.insert(Name::from_str("smartdns.").unwrap());
+            set.insert(Name::from_str("whoami.").unwrap());
             set
         };
 
-        Self { catalog, ptr_set }
+        Self {
+            catalog,
+            server_net,
+            server_names,
+        }
     }
 }
 
@@ -56,11 +74,22 @@ impl Middleware<DnsContext, DnsRequest, DnsResponse, DnsError> for DnsZoneMiddle
         let name = query.name();
         let rtype = query.query_type();
 
-        if rtype == RecordType::PTR && self.ptr_set.contains(name) {
-            return Ok(Lookup::from_rdata(
-                req.query().original().to_owned(),
-                RData::PTR(PTR(ctx.cfg().server_name())),
-            ));
+        if rtype == RecordType::PTR {
+            let mut is_current_server = false;
+            let name: &Name = name.borrow();
+
+            if self.server_names.contains(name) {
+                is_current_server = true;
+            } else if let Ok(net) = name.parse_arpa_name() {
+                is_current_server = self.server_net.iter().any(|ip| net.contains(ip));
+            }
+
+            if is_current_server {
+                return Ok(Lookup::from_rdata(
+                    req.query().original().to_owned(),
+                    RData::PTR(PTR(ctx.cfg().server_name())),
+                ));
+            }
         };
 
         if let Some(authority) = self.catalog.find(name) {
@@ -139,5 +168,36 @@ impl Catalog {
                     None
                 }
             })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    
+
+    use crate::infra::ipset::IpSet;
+
+    use super::*;
+
+    #[test]
+    fn test_arpa() {
+        let local_net = IpSet::new(vec![
+            "::1/128".parse().unwrap(),
+            "192.168.1.1/32".parse().unwrap(),
+        ]);
+
+        let name1: Name =
+            "1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa."
+                .parse()
+                .unwrap();
+
+        let net1 = name1.parse_arpa_name().unwrap();
+
+        assert!(local_net.contains(&net1));
+
+        let name2: Name = "1.168.192.in-addr.arpa.".parse().unwrap();
+        let net2 = name2.parse_arpa_name().unwrap();
+
+        assert!(local_net.iter().any(|net| net2.contains(net)));
     }
 }
